c938502164ab094fe3971e1228806d30c3e3f936
Carrie Segal <chsegal@umail.ucsb.edu>
2021-10-06 11:53:09 -0700

    2 neurons

--- two-neurons.py
+++ two-neurons.py
@@ -39,67 +39,64 @@ def phase_automata(driving_symbol='0',number_of_symbols=3,id_of_starting_symbol=
             j += 1
         i += 1
     ending_state = state
-    #print(code, ending_state)
     return code, ending_state
 
-model = nengo.Network(label='Two Neurons',seed=91195)
+
+model = nengo.Network(label='Two Neurons', seed=91195)
 
 with model:
     with model:
         neurons = nengo.Ensemble(
-            2,
-            dimensions=3,  # Represent a scalar
+            2,  # Number of neurons
+            dimensions=3,  # each neuron is connected to all (3) input channels.
             # Set intercept to 0.5
-            intercepts=Uniform(-0.00001, 0.00001),# Set the intercepts at 0.00001
-            neuron_type=nengo.LIF(min_voltage=0, tau_ref=0.0000000005, tau_rc=0.00000001),
-            # Set tau_ref= or tau_rc = here to 
-            # change those 
-            # parms 
-            # for the 
+            intercepts=Uniform(-0.00001, 0.00001),  # Set the intercepts at 0.00001 (threshold for Soma voltage)
+            neuron_type=nengo.LIF(min_voltage=0, tau_ref=0.0000000005, tau_rc=0.00000001),  # Specify type of neuron
+            # Set tau_ref= or tau_rc = here to
+            # change those
+            # parms
+            # for the
             # neurons.
-            # Set the maximum firing rate of the neuron 500Mhz
-            max_rates=Uniform(500000000,500000000),
-            # Set the neuron's firing rate to increase for 2 combinations of input
-            encoders=[[-1,-1,1],[1,-1,-1]],
+            max_rates=Uniform(500e+6, 500e+6),             # Set the maximum firing rate of the neuron 500Mhz
+            # Set the neuron's firing rate to increase for 2 combinations of 3 channel input.
+            encoders=[[-1, -1, 1], [1, -1, -1]],
         )
-        
 
-threeChannels, end_channel = phase_automata(driving_symbol="1",probability_of_transition=False)
+threeChannels, end_channel = phase_automata(driving_symbol="1", probability_of_transition=True)
 print(threeChannels)
-tC = threeChannels.transpose((1,0))
-    
+tC = threeChannels.transpose((1, 0))
+
 with model:
-    #sin = nengo.Node(lambda t: np.sin(8 * t))
-    input_signal = nengo.Node(PresentInput(tC, presentation_time=0.0000001))
-    
+    input_signal = nengo.Node(PresentInput(tC, presentation_time=1e-7))
+
 with model:
-    nengo.Connection(input_signal, neurons, synapse=0.01)
-    
+    nengo.Connection(input_signal, neurons, synapse=1) 
+
+
 with model:
-    sin_probe = nengo.Probe(input_signal)  # The original input
+    input_probe = nengo.Probe(input_signal)  # The original input
     spikes = nengo.Probe(neurons.neurons)  # Raw spikes from each neuron
     # Subthreshold soma voltages of the neurons
     voltage = nengo.Probe(neurons.neurons, 'voltage')
     # Spikes filtered by a 10ms post-synaptic filter
-    filtered = nengo.Probe(neurons, synapse=0.00000000001)
+    filtered = nengo.Probe(neurons, synapse=1e-11)
     
-with nengo.Simulator(model, dt=0.00000001) as sim:  # Create a simulator
-    sim.run(0.001)  # Run it for 1 second
+with nengo.Simulator(model, dt=1e-8) as sim:  # Create a simulator
+    sim.run(10000e-9)  # Run it for 10k nanosecond
     
 t = sim.trange()
 
-plot_range = 100 # index 
+plot_range = 100  # index
 # Plot the decoded output of the ensemble
 plt.figure()
-#plt.plot(t, sim.data[filtered])
-plt.plot(t, sim.data[sin_probe])
+plt.plot(t, sim.data[input_probe])
 plt.xlim(0, t[plot_range])
 plt.xlabel("Time (s)")
-plt.title("Input probe for "+ str(plot_range)+ " timesteps")
+plt.title("Input probe for " + str(plot_range) + " timesteps")
 plt.savefig("fig/two_neurons_input_probe.png")
 plt.clf()
 plt.figure()
-plt.title("Neurons filtered probe for "+ str(plot_range)+ " timesteps")
+plt.title("Neurons filtered probe for " + str(plot_range) + " timesteps")
 plt.plot(t, sim.data[filtered])
 plt.xlabel("Time (s)")
 plt.xlim(0, t[plot_range])
@@ -108,19 +105,18 @@ plt.savefig("fig/two_neurons_filtered.png")
 # Plot the spiking output of the ensemble
 plt.figure(figsize=(10, 4))
 plt.title("Neuron Spikes")
-plt.subplot(2, 2, 1)
+plt.subplot(1, 2, 1)
 plt.xlabel("Time (s)")
 rasterplot(t[0:plot_range], sim.data[spikes][0:plot_range], colors=[(1, 0, 0), (0, 0, 0)])
 plt.yticks((1, 2), ("On neuron", "Off neuron"))
 plt.ylim(2.5, 0.5)
 
 # Plot the soma voltages of the neurons
-plt.subplot(2, 2, 2)
+plt.subplot(1, 2, 2)
 plt.title("Neuron Soma Voltage")
 plt.plot(t, sim.data[voltage][:, 0] + 1, 'r')
 plt.plot(t, sim.data[voltage][:, 1], 'k')
 plt.xlabel("Time (s)")
 plt.yticks(())
-#plt.axis([0, 1, 0, 2])
 plt.subplots_adjust(wspace=0.05)
 plt.savefig("fig/two_neurons.png")
