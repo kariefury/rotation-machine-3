b4c90645f49d2d5af9e1ebbbd7e94c743c835ceb
Carrie Segal <chsegal@umail.ucsb.edu>
2021-10-07 10:25:55 -0700

    3 layer network

--- three-layers.py
+++ three-layers.py
@@ -42,7 +42,7 @@ def phase_automata(driving_symbol='0',number_of_symbols=3,id_of_starting_symbol=
     return code, ending_state
 
 
-model = nengo.Network(label='Two Layers', seed=91195)
+model = nengo.Network(label='Three Layers', seed=91195)
 
 with model:
     with model:
@@ -77,8 +77,26 @@ with model:
             # Set the neuron's firing rate to increase for 2 combinations of 3 channel input.
             encoders=[[1, -1, -1], [-1, 1, -1], [-1, -1, 1], [ -1, -1, -1], [1, 1, 1], [1, 1, -1]],
         )
+        
+        neuronsL3 = nengo.Ensemble(
+            2,  # Number of neurons
+            dimensions=3,
+            # Set intercept to 0.5
+            intercepts=Uniform(-0.00001, 0.00001),  # Set the intercepts at 0.00001 (threshold for Soma voltage)
+            neuron_type=nengo.LIF(min_voltage=0, tau_ref=0.0000000005, tau_rc=0.00000001),  # Specify type of neuron
+            # Set tau_ref= or tau_rc = here to
+            # change those
+            # parms
+            # for the
+            # neurons.
+            max_rates=Uniform(500e+6, 500e+6),  # Set the maximum firing rate of the neuron 500Mhz
+            # Set the neuron's firing rate to increase for 2 combinations of 3 channel input.
+            encoders=[[1, -1, -1], [-1, -1, 1]],
+        )
 
-threeChannels, end_channel = phase_automata(driving_symbol="1", probability_of_transition=True,timesteps=9)
+driving_symbol = "0"
+noise = True
+threeChannels, end_channel = phase_automata(driving_symbol=driving_symbol, probability_of_transition=noise,timesteps=90)
 print(threeChannels)
 tC = threeChannels.transpose((1, 0))
 print(tC)
@@ -88,16 +106,17 @@ with model:
 with model:
     nengo.Connection(input_signal, neurons, synapse=None)
     nengo.Connection(neurons, neuronsL2, synapse=None)
+    nengo.Connection(neuronsL2,neuronsL3,synapse=None)
 
 
 with model:
     input_probe = nengo.Probe(input_signal)  # The original input
-    spikesL2 = nengo.Probe(neuronsL2.neurons)  # Raw spikes from each neuron
+    spikesL3 = nengo.Probe(neuronsL3.neurons)  # Raw spikes from each neuron
     # Subthreshold soma voltages of the neurons
     #voltage = nengo.Probe(neurons.neurons, 'voltage')
-    voltageL2 = nengo.Probe(neuronsL2.neurons, 'voltage')
+    voltageL3 = nengo.Probe(neuronsL3.neurons, 'voltage')
     # Spikes filtered by a 10ms post-synaptic filter
-    filteredL2 = nengo.Probe(neuronsL2, synapse=1e-11)
+    filteredL3 = nengo.Probe(neuronsL3, synapse=1e-8)
     
 with nengo.Simulator(model, dt=1e-8) as sim:  # Create a simulator
     sim.run(1000000e-9)  # Run it for 10k nanosecond
@@ -110,35 +129,33 @@ plt.figure()
 plt.plot(t, sim.data[input_probe])
 plt.xlim(0, t[plot_range])
 plt.xlabel("Time (s)")
-plt.title("Input probe for " + str(plot_range) + " timesteps")
-plt.savefig("fig/two_layers_input_probe.png")
+plt.title("Input probe for " + str(plot_range) + " timesteps " +"Driving Symbol:"+driving_symbol+" Noise "
+                                                                                                        ""+str(noise))
+plt.savefig("fig/three_layers_input_probe"+driving_symbol+".png")
 plt.clf()
 plt.figure()
-plt.title("Neurons filtered probe for " + str(plot_range) + " timesteps")
-plt.plot(t, sim.data[filteredL2])
+plt.title("Neurons filtered probe for " + str(plot_range) + " timesteps Driving Symbol:"+driving_symbol+" Noise "
+                                                                                                        ""+str(noise))
+plt.plot(t, sim.data[filteredL3])
 plt.xlabel("Time (s)")
 plt.xlim(0, t[plot_range])
-plt.savefig("fig/two_layers_filtered.png")
+plt.savefig("fig/three_layers_filtered"+driving_symbol+".png")
 
 # Plot the spiking output of the ensemble
 plt.figure(figsize=(10, 4))
-plt.title("Neuron Spikes")
+plt.title("Neuron Spikes Driving Symbol "+driving_symbol+" Noise "+str(noise))
 plt.subplot(1, 2, 1)
 plt.xlabel("Time (s)")
-rasterplot(t[0:plot_range], sim.data[spikesL2][0:plot_range], colors=['y', 'm', 'k', 'r','b','g'])
-plt.yticks((1,2,3,4,5,6), ("0" ,"1" ,"2","3", "4","5"))
-plt.ylim(6.5, 0.5)
+rasterplot(t[0:plot_range], sim.data[spikesL3][0:plot_range], colors=['y', 'm'])
+plt.yticks((1,2), ("0" ,"1" ))
+plt.ylim(2.5, 0.5)
 
 # Plot the soma voltages of the neurons
 plt.subplot(1, 2, 2)
-plt.title("Neuron Soma Voltage")
-plt.plot(t, sim.data[voltageL2][:, 5] + 0, 'g')
-plt.plot(t, sim.data[voltageL2][:, 4] + 1, 'b')
-plt.plot(t, sim.data[voltageL2][:, 3] + 2, 'r')
-plt.plot(t, sim.data[voltageL2][:, 2]+3, 'k')
-plt.plot(t, sim.data[voltageL2][:, 1]+4, 'm')
-plt.plot(t, sim.data[voltageL2][:, 0]+5, 'y')
+plt.title("Neuron Soma Voltage Driving Symbol "+driving_symbol+" Noise "+str(noise))
+plt.plot(t, sim.data[voltageL3][:, 1]+4, 'm')
+plt.plot(t, sim.data[voltageL3][:, 0]+5, 'y')
 plt.xlabel("Time (s)")
 plt.yticks(())
 plt.subplots_adjust(wspace=0.05)
-plt.savefig("fig/two_layers.png")
+plt.savefig("fig/three_layers"+driving_symbol+".png")
