45f4d63c8a6962f2dbbe237c58b10c4da4e33f2a
Carrie Segal <carrie@skyberrys.com>
2021-10-19 15:40:50 -0700

    learning w feedback

--- three-layers-feedback.py
+++ three-layers-feedback.py
@@ -7,6 +7,44 @@ from nengo.utils.matplotlib import rasterplot
 from nengo.processes import PresentInput
 from nengo.utils.ensemble import tuning_curves
 
+def preset_timing_plot():
+    print("sim.dt",sim.dt)
+    print("Symbol presentation time",pt)
+    print("Label length", label_length)
+    print("layer 1 Tau RC",layer1.neuron_type.tau_rc)
+    print("layer1 refractory period",layer1.neuron_type.tau_ref)
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+    
+    people = ('Sim dt', 'Pulse Time', 'Pulse Spacing', 'Rotation Time', 'Label Time', 'L1 Tau RC', 'L1 Tau Ref', 
+    'L2 Tau RC', 
+              "L2 Tau Ref", 
+              "L3 Tau RC", "L3 Tau REf")
+    y_pos = np.arange(len(people))
+    print (y_pos)
+    performance = np.zeros(len(people),dtype=float)
+    lbs = [1,2,3,4,5,6,7,8,9,10]
+    #performance = 3 + 10 * np.random.rand(len(people))
+    performance[0] = sim.dt
+    performance[1] = pt
+    performance[2] = 0.0
+    performance[3] = ts*pt
+    performance[4] = label_length*pt
+    performance[5] = layer1.neuron_type.tau_rc
+    performance[6] = layer1.neuron_type.tau_ref
+    performance[7] = layer2.neuron_type.tau_rc
+    performance[8] = layer2.neuron_type.tau_ref
+    performance[9] = layer3.neuron_type.tau_rc
+    performance[10] = layer3.neuron_type.tau_ref
+    ax.barh(y_pos, performance)
+    ax.set_yticks(y_pos)
+    #plt.xscale("log")
+    ax.set_yticklabels(people)
+    ax.invert_yaxis()  # labels read top-to-bottom
+    ax.set_xlabel('Time (s)')
+    ax.set_title('Preset Timing Parameters')
+
+    plt.show()
 
 def phase_automata(driving_symbol='0', number_of_symbols=3, id_of_starting_symbol=0, timesteps=9,
                    probability_of_transition=False):
@@ -46,8 +84,8 @@ def phase_automata(driving_symbol='0', number_of_symbols=3, id_of_starting_symbo
 
 reseed = 91332 #91323 #91280 #91274 # 91264 # 91254  # 91273
 good = False
-number_of_samples = 6
-ts = 6  # number of timesteps to hold a driving symbol constant for.
+number_of_samples = 64
+ts = 9  # number of timesteps to hold a driving symbol constant for.
 pb = False
 pt = 3e-2 # seconds to present each step of input
 label_length = ts * 3 # In Timesteps, multiply by dt to get actual length of time
@@ -74,7 +112,7 @@ plt.plot(t,bothLabels+2.0,color="black",label="label")
 plt.legend()
 plt.savefig("fig/input_pattern_example_probTran_"+str(pb)+"_padded_zeros_true.png")
 i = 1
-bitstring = "0101100001010"
+bitstring = "0101100001010000000111111101010101010011001100011100101010000111100000000000000000000000000000000000000000000000000000000000000000"
 w = 0
 while i < number_of_samples:
     threeChannelsOF1, end_channel = phase_automata(driving_symbol=bitstring[w], probability_of_transition=pb, 
@@ -111,188 +149,153 @@ tC = bothPatterns.transpose((1, 0))
 labels = bothLabels
 
 
-while not good:
-    model = nengo.Network(label='Three Layers with feedback', seed=reseed)
-    num_neurons_l1 = 4
-    num_neurons_l2 = 6
-    num_neurons_l3 = 2
+#while not good:
+model = nengo.Network(label='Three Layers with feedback', seed=reseed)
+num_neurons_l1 = 4
+num_neurons_l2 = 6
+num_neurons_l3 = 2
+with model:
     with model:
-        with model:
-            layer1 = nengo.Ensemble(
-                num_neurons_l1,  # Number of neurons
-                dimensions=3,  # each neuron is connected to all (3) input channels.
-                # Set intercept to 0.5
-                neuron_type=nengo.LIF(min_voltage=0, tau_ref=5e-2, tau_rc=1e-2),  # Specify type of neuron
-                max_rates=Uniform(1 / 6e-2, 1 / 6e-2),  # Set the maximum firing rate of the neuron 500Mhz
-            )
+        layer1 = nengo.Ensemble(
+            num_neurons_l1,  # Number of neurons
+            dimensions=3,  # each neuron is connected to all (3) input channels.
+            # Set intercept to 0.5
+            neuron_type=nengo.LIF(min_voltage=0, tau_ref=5e-2, tau_rc=1e-2),  # Specify type of neuron
+            max_rates=Uniform(1 / 6e-2, 1 / 6e-2),  # Set the maximum firing rate of the neuron 500Mhz
+        )
 
-            layer2 = nengo.Ensemble(
-                num_neurons_l2,  # Number of neurons
-                dimensions=3,
-                # Set intercept to 0.5
-                neuron_type=nengo.LIF(min_voltage=0, tau_ref=5e-2, tau_rc=1e-2),  # Specify type of neuron
-                max_rates=Uniform(1 / 6e-2, 1 / 6e-2),  # Set the maximum firing rate of the neuron 500Mhz
-            )
+        layer2 = nengo.Ensemble(
+            num_neurons_l2,  # Number of neurons
+            dimensions=3,
+            # Set intercept to 0.5
+            neuron_type=nengo.LIF(min_voltage=0, tau_ref=5e-2, tau_rc=1e-2),  # Specify type of neuron
+            max_rates=Uniform(1 / 6e-2, 1 / 6e-2),  # Set the maximum firing rate of the neuron 500Mhz
+        )
 
-            layer3 = nengo.Ensemble(
-                num_neurons_l3,  # Number of neurons
-                dimensions=3,
-                # Set intercept to 0.5
-                neuron_type=nengo.LIF(min_voltage=0, tau_ref=5e-2, tau_rc=1e-2),  # Specify type of neuron
-                max_rates=Uniform(1 / 6e-2, 1 / 6e-2),  # Set the maximum firing rate of the neuron 500Mhz
-            )
+        layer3 = nengo.Ensemble(
+            num_neurons_l3,  # Number of neurons
+            dimensions=3,
+            # Set intercept to 0.5
+            neuron_type=nengo.LIF(min_voltage=0, tau_ref=5e-1, tau_rc=1e-1),  # Specify type of neuron
+            max_rates=Uniform(1 / 6e-1, 1 / 6e-1),  # Set the maximum firing rate of the neuron 500Mhz
+        )
 
-    with model:
-        input_signal = nengo.Node(PresentInput(tC, presentation_time=pt))
-        input_keys = nengo.Node(PresentInput(labels, presentation_time=pt))
 
-    with model:
-        nengo.Connection(input_signal, layer1, synapse=None)
-        nengo.Connection(layer1, layer2, synapse=1e-1)
-        nengo.Connection(layer2, layer1, synapse=ts*1e-1)
-        nengo.Connection(layer2, layer3, synapse=1e-2)
-        nengo.Connection(layer3, layer2, synapse=ts*1e-1)
+with model:
+    input_signal = nengo.Node(PresentInput(tC, presentation_time=pt))
+    input_keys = nengo.Node(PresentInput(labels, presentation_time=pt))
 
-    simT = 180
-    with model:
-        input_probe = nengo.Probe(input_signal)  # The original input
-        spikes = nengo.Probe(layer1.neurons)  # Raw spikes from each neuron
-        # Subthreshold soma voltages of the neurons
-        
-        #voltage = nengo.Probe(layer1.neurons, 'voltage')
-        # Spikes filtered by a 10ms post-synaptic filter
-        filteredl1 = nengo.Probe(layer1.neurons, 'voltage', synapse=2e-2)
-        filteredl2 = nengo.Probe(layer2.neurons, 'voltage', synapse=2e-2)
-        filteredl3 = nengo.Probe(layer3.neurons, 'voltage', synapse=2e-2)
-        
-        # Setup probes
-        p_keys = nengo.Probe(input_keys, synapse=None, label="p_keys")
-        p_values = nengo.Probe(input_signal, synapse=None, label="p_values")
-        
-    with nengo.Simulator(model) as sim:  # Create a simulator
-        sim.run(simT)  # Run 
+with model:
+    nengo.Connection(input_signal, layer1, synapse=None,label="input signals to layer 1")
+    nengo.Connection(layer1, layer2, synapse=1e-1, label="layer 1 to layer2")
+    nengo.Connection(layer2, layer1, synapse=ts*1e-1, label="layer 2 to layer 1")
+    conn = nengo.Connection(layer2, layer3, synapse=1e-2, label="layer 2 to layer 3")
+    nengo.Connection(layer3, layer2, synapse=ts*1e-1, label="layer3 to layer2")
 
-    t = sim.trange()
-
-    plot_range = -1  # index
-
-    train = t <= simT / 2
-    test = ~train
-
+simT = 10
+with model:
+    input_probe = nengo.Probe(input_signal)  # The original input
+    spikes = nengo.Probe(layer1.neurons)  # Raw spikes from each neuron
+    # Subthreshold soma voltages of the neurons
     
+    #voltage = nengo.Probe(layer1.neurons, 'voltage')
+    # Spikes filtered by a 10ms post-synaptic filter
+    filteredl1 = nengo.Probe(layer1.neurons, 'voltage', synapse=3e-2)
+    filteredl2 = nengo.Probe(layer2.neurons, 'voltage', synapse=3e-2)
+    filteredl3 = nengo.Probe(layer3.neurons, 'voltage', synapse=3e-2)
     
-    # plt.figure()
-    # plt.title("Value Error During Training")
-    # plt.plot(t[train], sim.data[p_error][train])
-    # plt.show()
+    # Setup probes
+    p_keys = nengo.Probe(input_keys, synapse=None, label="p_keys")
+    p_values = nengo.Probe(input_signal, synapse=None, label="p_values")
+    
+with nengo.Simulator(model) as sim:  # Create a simulator
+    sim.run(simT)  # Run 
 
-    plt.figure()
-    plt.title("Filtered L1 output")
-    # print(np.shape(sim.data[p_recall][test][0:,0:1]), np.shape(sim.data[p_values][test][0:,0:1]))
-    # plt.plot(t, sim.data[p_recall][0:,0:1])# - sim.data[p_values][0:,0:1])
-    
-    plt.plot(t, sim.data[input_probe] + 2)
-    plt.plot(t, sim.data[p_keys] + 0, color="black")
-    print(np.shape(sim.data[filteredl1]))
-    plt.plot(t, sim.data[filteredl1][0:, 0:1] + 3, color="b")
-    plt.plot(t, sim.data[filteredl1][0:, 1:2] + 4, color="y")
-    plt.plot(t, sim.data[filteredl1][0:, 2:3] + 5, color="k")
-    plt.plot(t, sim.data[filteredl1][0:, 3:4] + 6, color="m")
-    plt.savefig('fig/3layersfeedbck_neuronsl1_4.png')
+t = sim.trange()
+
+plot_range = -1  # index
+
+train = t <= simT / 2
+test = ~train
+
+
+
+# plt.figure()
+# plt.title("Value Error During Training")
+# plt.plot(t[train], sim.data[p_error][train])
+# plt.show()
+
+plt.figure()
+plt.title("Filtered L1 output")
+# print(np.shape(sim.data[p_recall][test][0:,0:1]), np.shape(sim.data[p_values][test][0:,0:1]))
+# plt.plot(t, sim.data[p_recall][0:,0:1])# - sim.data[p_values][0:,0:1])
+
+plt.plot(t, sim.data[input_probe] + 2)
+plt.plot(t, sim.data[p_keys] + 0, color="black")
+print(np.shape(sim.data[filteredl1]))
+plt.plot(t, sim.data[filteredl1][0:, 0:1] + 3, color="b")
+plt.plot(t, sim.data[filteredl1][0:, 1:2] + 4, color="y")
+plt.plot(t, sim.data[filteredl1][0:, 2:3] + 5, color="k")
+plt.plot(t, sim.data[filteredl1][0:, 3:4] + 6, color="m")
+plt.savefig('fig/3layersfeedbck_neuronsl1_4.png')
 #    plt.plot(t, sim.data[filtered][0:, 4:5] + 7, color="r")
 #    plt.plot(t, sim.data[filtered][0:, 5:6] + 8, color="g")
 #    plt.plot(t, sim.data[filtered][0:, 6:7] + 9, color="#aabbcc")
 
-    plt.figure()
-    plt.title("Filtered L2 output")
-    # print(np.shape(sim.data[p_recall][test][0:,0:1]), np.shape(sim.data[p_values][test][0:,0:1]))
-    # plt.plot(t, sim.data[p_recall][0:,0:1])# - sim.data[p_values][0:,0:1])
-    plt.plot(t, sim.data[input_probe] + 2)
-    plt.plot(t, sim.data[p_keys] + 0, color="black")
-    print(np.shape(sim.data[filteredl2]))
-    plt.plot(t, sim.data[filteredl2][0:, 0:1] + 3, color="b")
-    plt.plot(t, sim.data[filteredl2][0:, 1:2] + 4, color="y")
-    plt.plot(t, sim.data[filteredl2][0:, 2:3] + 5, color="k")
-    plt.plot(t, sim.data[filteredl2][0:, 3:4] + 6, color="m")
-    plt.plot(t, sim.data[filteredl2][0:, 4:5] + 7, color="r")
-    plt.plot(t, sim.data[filteredl2][0:, 5:6] + 8, color="g")
-    plt.savefig('fig/3layersfeedbck_neuronsl2_6.png')
+plt.figure()
+plt.title("Filtered L2 output")
+# print(np.shape(sim.data[p_recall][test][0:,0:1]), np.shape(sim.data[p_values][test][0:,0:1]))
+# plt.plot(t, sim.data[p_recall][0:,0:1])# - sim.data[p_values][0:,0:1])
+plt.plot(t, sim.data[input_probe] + 2)
+plt.plot(t, sim.data[p_keys] + 0, color="black")
+print(np.shape(sim.data[filteredl2]))
+plt.plot(t, sim.data[filteredl2][0:, 0:1] + 3, color="b")
+plt.plot(t, sim.data[filteredl2][0:, 1:2] + 4, color="y")
+plt.plot(t, sim.data[filteredl2][0:, 2:3] + 5, color="k")
+plt.plot(t, sim.data[filteredl2][0:, 3:4] + 6, color="m")
+plt.plot(t, sim.data[filteredl2][0:, 4:5] + 7, color="r")
+plt.plot(t, sim.data[filteredl2][0:, 5:6] + 8, color="g")
+plt.savefig('fig/3layersfeedbck_neuronsl2_6.png')
 
-    plt.figure()
-    plt.title("Filtered L3 output")
+plt.figure()
+plt.title("Filtered L3 output")
 
-    plt.plot(t[test], sim.data[p_keys][test] + 0, color="black")
-    plt.plot(t[test], sim.data[input_probe][test] + 2)
-    print(np.shape(sim.data[filteredl2]))
-    plt.plot(t[test], sim.data[filteredl2][test][0:, 0:1] + 3, color="b")
-    plt.plot(t[test], sim.data[filteredl2][test][0:, 1:2] + 4, color="y")
-    plt.savefig('fig/3layersfeedbck_neuronsl3_2_'+str(reseed)+'.png')
+plt.plot(t[test], sim.data[p_keys][test] + 0, color="black")
+plt.plot(t[test], sim.data[input_probe][test] + 2)
+print(np.shape(sim.data[filteredl2]))
+plt.plot(t[test], sim.data[filteredl2][test][0:, 0:1] + 3, color="b")
+plt.plot(t[test], sim.data[filteredl2][test][0:, 1:2] + 4, color="y")
+plt.savefig('fig/3layersfeedbck_neuronsl3_2_'+str(reseed)+'.png')
 
-    plt.clf()
-    
-    plt.figure()
-    time_input_signal = t <= pt*ts
-    plt.title("single timesteps of input")
-    plt.plot(t[time_input_signal],sim.data[input_probe][time_input_signal][0: , 0:1], color="blue")
-    plt.plot(t[time_input_signal],sim.data[input_probe][time_input_signal][0: , 1:2]+2.1, color="orange")
-    plt.plot(t[time_input_signal],sim.data[input_probe][time_input_signal][0: , 2:3] + 4.2 , color="green")
-    plt.savefig('fig/inputTiming.png')
-    i = 0
-    
-    best_neuron_value = np.sum((sim.data[filteredl3][0:, 0:0 + 1]+1.0) - (sim.data[p_keys]*2.0))
-    print(best_neuron_value)
-    best_neuron_index = 0
-    while i < num_neurons_l3:
-        sum = np.sum((sim.data[filteredl3][0:, i:i + 1]+1.0) - (sim.data[p_keys]*2.0))
-        # print(i, sum)
+plt.clf()
 
-        if (sum < best_neuron_value):
-            best_neuron_index = i
-            best_neuron_value = sum
-        print(best_neuron_value, best_neuron_index, reseed)
-        i += 1
-    if (np.abs(best_neuron_value) < 20000):
-        good = True
-    else:
-        reseed += 1
-    print("sim.dt",sim.dt)
-    print("Symbol presentation time",pt)
-    print("Label length", label_length)
-    print("layer 1 Tau RC",layer1.neuron_type.tau_rc)
-    print("layer1 refractory period",layer1.neuron_type.tau_ref)
-    plt.rcdefaults()
-    fig, ax = plt.subplots()
-    
-    people = ('Sim dt', 'Pulse Time', 'Pulse Spacing', 'Rotation Time', 'Label Time', 'L1 Tau RC', 'L1 Tau Ref', 
-    'L2 Tau RC', 
-              "L2 Tau Ref", 
-              "L3 Tau RC", "L3 Tau REf")
-    y_pos = np.arange(len(people))
-    print (y_pos)
-    performance = np.zeros(len(people),dtype=float)
-    lbs = [1,2,3,4,5,6,7,8,9,10]
-    #performance = 3 + 10 * np.random.rand(len(people))
-    performance[0] = sim.dt
-    performance[1] = pt
-    performance[2] = 0.0
-    performance[3] = ts*pt
-    performance[4] = label_length*pt
-    performance[5] = layer1.neuron_type.tau_rc
-    performance[6] = layer1.neuron_type.tau_ref
-    performance[7] = layer2.neuron_type.tau_rc
-    performance[8] = layer2.neuron_type.tau_ref
-    performance[9] = layer3.neuron_type.tau_rc
-    performance[10] = layer3.neuron_type.tau_ref
-    ax.barh(y_pos, performance)
-    ax.set_yticks(y_pos)
-    #plt.xscale("log")
-    ax.set_yticklabels(people)
-    ax.invert_yaxis()  # labels read top-to-bottom
-    ax.set_xlabel('Time (s)')
-    ax.set_title('Preset Timing Parameters')
+plt.figure()
+time_input_signal = t <= pt*ts
+plt.title("single timesteps of input")
+plt.plot(t[time_input_signal],sim.data[input_probe][time_input_signal][0: , 0:1], color="blue")
+plt.plot(t[time_input_signal],sim.data[input_probe][time_input_signal][0: , 1:2]+2.1, color="green")
+plt.plot(t[time_input_signal],sim.data[input_probe][time_input_signal][0: , 2:3] + 4.2 , color="orange")
+plt.savefig('fig/inputTiming.png')
+i = 0
 
-    plt.show()
-    
+best_neuron_value = np.sum((sim.data[filteredl3][0:, 0:0 + 1]+1.0) - (sim.data[p_keys]*2.0))
+print(best_neuron_value)
+best_neuron_index = 0
+while i < num_neurons_l3:
+    sum = np.sum((sim.data[filteredl3][0:, i:i + 1]+1.0) - (sim.data[p_keys]*2.0))
+    # print(i, sum)
+
+    if (sum < best_neuron_value):
+        best_neuron_index = i
+        best_neuron_value = sum
+    print(best_neuron_value, best_neuron_index, reseed)
+    i += 1
+if (np.abs(best_neuron_value) < 20000):
+    good = True
+else:
+    reseed += 1
+
+#    preset_timing_plot()    
     good = True
 
 plt.figure()
@@ -304,3 +307,33 @@ plt.plot(t, sim.data[input_probe] + 2)
 plt.plot(t, sim.data[filteredl3][0:, best_neuron_index:best_neuron_index + 1] + 3, color="black")
 plt.savefig('fig/3layersfeedbck_best_neuron_index_l3.png')
 
+
+
+with model:
+    print(model)
+    print(model.all_connections)
+    #nengo.Connection(input_signal, layer1, synapse=None)
+    #conn = nengo.Connection(layer1, layer2, synapse=1e-1)
+    #nengo.Connection(layer2, layer1, synapse=ts*1e-1)
+    #nengo.Connection(layer2, layer3, synapse=1e-2)
+    #nengo.Connection(layer3, layer2, synapse=ts*1e-1)
+    error = nengo.Ensemble(40,dimensions=1,label="error network")
+    error_values = nengo.Probe(error, synapse=None, label="error_values")
+    nengo.Connection(input_keys,error,transform=1,synapse=3e-3, label="input keys to error")
+    nengo.Connection(layer3,error,transform=[[-1,-1,-1]], synapse=ts*1e-3, label="layer3 to error network")
+    conn.learning_rule_type = nengo.PES(pre_synapse=9e-1)
+    nengo.Connection(error,conn.learning_rule,transform=[[1],[1],[1]], label="error network connect to learning rule")
+    print(model.all_connections)
+with nengo.Simulator(model) as sim:  # Create a simulator
+    sim.run(simT)  # Run 
+
+plt.figure()
+plt.title("Learned Filtered output")
+# print(np.shape(sim.data[p_recall][test][0:,0:1]), np.shape(sim.data[p_values][test][0:,0:1]))
+# plt.plot(t, sim.data[p_recall][0:,0:1])# - sim.data[p_values][0:,0:1])
+plt.plot(t, sim.data[error_values],color="red")
+plt.plot(t, sim.data[p_keys] + 0, color="g")
+plt.plot(t, sim.data[input_probe] + 2)
+plt.plot(t, sim.data[filteredl3][0:, best_neuron_index:best_neuron_index + 1] + 3, color="black")
+
+plt.savefig('fig/3layersfeedbck_best_neuron_index_l3_learned.png')
